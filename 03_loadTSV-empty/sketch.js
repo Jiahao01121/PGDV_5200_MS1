1a7eb5c884ce2420bbbe2beb70deacb68c702a9b
WillSu <jiahao01121@gmail.com>
2017-02-24 14:23:13 -0500

    a

--- 03_loadTSV-empty/sketch.js
+++ 03_loadTSV-empty/sketch.js
@@ -1,5 +1,26 @@
+
 function setup() {
+  createCanvas(windowWidth,windowHeight);
+  loadTable("groceries.tsv",'tsv','header',showData)
 }
 
 function showData(data) {
+var rows = data.getRowCount();
+var lineHeight = height /rows;
+  for(var i = 0; i < rows; i++){
+    var amount = data.get(i,0);
+    var unit = data.get(i,1);
+    var item = data.get(i,2);
+    var source = data.get(i,3);
+    console.log(amount);
+
+    if(source == "market"){
+      fill(255,0,0)
+    }
+    else{
+      fill(0)
+    }
+    text(`${amount} ${unit} ${item} ${source}`,width/2 ,20 +lineHeight*i)
+
+  }
 }
--- 04_displayTSV-empty/sketch.js
+++ 04_displayTSV-empty/sketch.js
@@ -1,3 +1,45 @@
 function setup(){
-	
+	createCanvas(windowWidth,windowHeight);
+	loadTable('data/LaborInNonAgricultSector.tsv','tsv','header',showData)
 }
+
+function showData(data){
+	console.log(data.get(7,3));
+	var rows = data.getRowCount();
+	var cols = data.getColumnCount();
+	var raw,col,min,max;
+
+	for(row = 0 ; row < rows ; row++){
+
+			for(col = 3 ; col < cols ; col++){
+					val = data.get(row, col);
+					val = float(val);
+
+					ellipse((width/25)*col,val,20,20)
+					console.log( val);
+			}
+	}
+
+
+}
+// function draw(){
+// 	background(255);
+//
+// 	var x = 20;
+// 	var y = 20;
+// 	var w = 400;
+// 	var h = 200;
+// 	fill(200);
+// 	rect(x,y,w,h);
+//
+//
+// 	fill(0);
+// 	ellipse(mouseX,mouseY,20,20);
+// 	ellipse(
+// 		map(mouseX,0,width,x,w+x),
+// 		map(mouseY,0,width,y,w+y),
+// 		20,
+// 		20
+// 	)
+//
+// }
